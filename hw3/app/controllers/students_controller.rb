d2a8c75354271ae737cc726501b2de4989a1eb07
KEV-PC\Kevin <kngovand@gmail.com>
2018-10-25 17:31:10 -0600

    Added features: navBar with bootstrap, Devise gem for Authentication.

--- hw3/app/controllers/students_controller.rb
+++ hw3/app/controllers/students_controller.rb
@@ -1,6 +1,7 @@
 class StudentsController < ApplicationController
   before_action :set_student, only: [:show, :edit, :update, :destroy]
-
+  before_action :authenticate_user!
+  
   # GET /students
   # GET /students.json
   def index
