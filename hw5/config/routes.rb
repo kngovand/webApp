a78374b2af28f2dac9eb8988a92767b8941a6e58
KEV-PC\Kevin <kngovand@gmail.com>
2018-12-02 15:21:57 -0700

    Prepping for Hw5.

---
+++ hw5/config/routes.rb
@@ -0,0 +1,27 @@
+Rails.application.routes.draw do
+
+  devise_for :users
+  
+  root 'welcome#index'
+
+  resources :students  do
+    collection do
+      get 'search'
+    end
+  end
+
+  resources :sections  do
+    collection do
+      get 'search'
+    end
+  end
+
+  resources :courses do
+    collection do
+      get 'search'
+    end
+  end
+
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
---
+++ hw5/test/models/section_test.rb
@@ -0,0 +1,5 @@
+require 'test_helper'
+
+class SectionTest < ActiveSupport::TestCase
+
+end
